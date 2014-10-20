9460cb3352f045fa83dfce68e3fd9af7e73537ba
antiapsurd <antiapsurd@antiapsurd.org>
2014-10-20 19:50:11 +0200

    plan & date

---
+++ workshop.py
@@ -0,0 +1,16 @@
+def python_workshop(date):
+    d = {
+    '08.09.2014': 'Radili tool-ing, napravili okvirni plan, upoznali se sa materijom, poceli tipove podataka',
+    '15.09.2014': 'Razjasnili nejasnoce oko domaceg string.py, ucili liste i pogledati dictionary za domaci, imali kratku samorefleksiju',
+    '22.09.2014': 'Radili domaci list.py, recnike i funkcije, igrali se sa debagerom',
+    '29.09.2014': 'Radili domaci wordcount.py, regularne izraze, poceli rekurzivne funkcije, puskice :)',
+    '06.10.2014': 'Radili OS module, igrali se sa fajlovima i direktorijumima',
+    '13.10.2014': 'Ponovili OS module, OOP, poceli obradjivati klase, objekte i metode',
+    }
+    if 0:
+      for k,v in d.items(): 
+        print (k,':', v)
+    if 1:
+      for k in d:
+        print(k, ':', d[k])
+python_workshop('stagod')
