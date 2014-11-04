78b4a10a9d90019d7d145e69a0f7b61a84db189e
Predrag Radovic <pedja@crackleware.nu>
2014-11-04 10:35:10 +0100

    fix webapp url in temperature sending shell script

--- temperature-monitoring/openwrt/root/send-temps.sh
+++ temperature-monitoring/openwrt/root/send-temps.sh
@@ -5,7 +5,7 @@ cd /root
 sleep 30
 
 while true; do
-	wget -q 'http://young-temple-8268.herokuapp.com/add?temp='`digitemp_DS9097 -a | tail -n1 | cut -d' ' -f7`
+	wget -q 'http://stark-savannah-9456.herokuapp.com/add?temp='`digitemp_DS9097 -a | tail -n1 | cut -d' ' -f7`
 	sleep 600
 done
 
