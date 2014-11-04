7fc02190a61a6a004e1c3913400bad2b55846683
Predrag Radovic <pedja@crackleware.nu>
2014-11-04 09:49:59 +0100

    fixed bad import

--- temperature-monitoring/tempmon-webapp/hello/views.py
+++ temperature-monitoring/tempmon-webapp/hello/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
+from .models import Temperature
 
 # Create your views here.
 def index(request):
