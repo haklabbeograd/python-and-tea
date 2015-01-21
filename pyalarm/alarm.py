dfbbc69039e76c06d2a539ea5e69a5252b922848
Predrag Radovic <pedja@crackleware.nu>
2015-01-21 13:01:11 +0100

    working on web UI

--- pyalarm/alarm.py
+++ pyalarm/alarm.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import sys
+
+from flask import Flask, request
 app = Flask(__name__)
 
 from datetime import datetime
@@ -6,6 +8,9 @@ from datetime import datetime
 import threading
 import time
 import subprocess
+
+import json
+import copy
 
 # from threading import Thread as Zeka
 
@@ -16,6 +21,24 @@ params = {
     'tip': 'zvuk',
     'sablon': 'beskonacno',
 }
+
+# from collections import namedtuple
+# class Params(namedtuple('Params', [
+#             'jacina',
+#             'vreme',
+#             'trajanje',
+#             'tip',
+#             'sablon',
+#             ])): pass
+# config = Params(**params)
+# print config
+# config2 = Params(jacina=0.4, vreme=40105, trajanje=20, tip='zvuk', sablon='beskonacno')
+# print config == config2
+# print config._asdict()
+# import json
+# print json.dumps(config._asdict())
+# sys.exit()
+
 
 def calc_time():
     h = int(params['vreme']/3600)
@@ -29,12 +52,40 @@ def index():
     return r'''
 <pre>
 jacina: %.0f%%
-vreme : %d:%02d:%02d
+vreme : %02d:%02d:%02d
 tip   : %s
 sablon: %s
 </pre>
 ''' % (params['jacina']*100, h, m, s, 
        params['tip'], params['sablon'])
+
+@app.route("/get_params")
+def get_params():
+    d = copy.copy(params)
+    d['vreme_comps'] = list(calc_time())
+    return json.dumps(d)
+#     return r'''
+# {"jacina": %r,
+# "vreme": %r,
+# "tip": %r,
+# "sablon": %r
+# }
+# ''' % (params['jacina'], params['vreme'], 
+#        params['tip'], params['sablon'])
+
+@app.route("/set_params", methods=['POST'])
+def set_params():
+    global params
+    d = request.get_json()
+    print type(d), d
+    params = d
+    return 'OK'
+
+@app.route("/ui.html")
+def ui_html(): return open('ui.html').read()
+
+@app.route("/jquery-2.1.1.min.js")
+def jquery_js(): return open('jquery-2.1.1.min.js').read()
 
 # @app.route("/page2/<param>")
 # def page2tttt(param):
@@ -66,7 +117,7 @@ def alarm_activator():
         should = alarm_should_be_on()
         active = proc != None
 
-        print (should, active)
+        # print (should, active)
  
         # if   (should, active) == (False, False):
         #     pass
@@ -85,9 +136,10 @@ def alarm_activator():
         time.sleep(1.0)
 
 if __name__ == "__main__":
-    params['vreme'] = current_ts() + 10
+    #params['vreme'] = current_ts() + 10
 
-    thr = threading.Thread(target=alarm_activator)
-    thr.start()
+    # thr = threading.Thread(target=alarm_activator)
+    # thr.setDaemon(True)
+    # thr.start()
 
-    app.run(debug=False)
+    app.run(debug=True)
